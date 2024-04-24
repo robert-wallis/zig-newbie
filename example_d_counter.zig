404a993e694c770aeab25a6ebef5dcd6f1c4cc6b
Robert Wallis <smilingrob@gmail.com>
2024-04-24 12:23:01 -0700

    I think I need to review what const means

---
+++ example_d_counter.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+
+test "bytesToHex" {
+    // Kristoff commented https://ziggit.dev/t/newbie-comptime-anytype-papercuts/4077/2?u=robert-wallis
+    // > The slice cases (D, E) didn’t work because []u8 is a mutable slice.
+    // > Had you used []const u8 it would have worked.
+    // > The slice has to be const because you’re taking a pointer from a temporary value
+    // > (the array returned by bytesToHex) and those values are implicitly const.
+    const input = "\xde\xad\xc0\xde";
+    const actual: []const u8 = &std.fmt.bytesToHex(input, .lower);
+    try std.testing.expectEqualStrings("deadc0de", actual);
+}
