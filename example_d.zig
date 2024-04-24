2f670f99d16c43760c96d548d462185071983485
Robert Wallis <smilingrob@gmail.com>
2024-04-24 12:52:26 -0700

    updating line numbers

--- example_d.zig
+++ example_d.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 // ❌ Fails
 //     % zig-0.12.0 test example_d.zig -freference-trace                                                                                                                                                                                              (main)
-//     example_d.zig:5:44: error: array literal requires address-of operator (&) to coerce to slice type '[]u8'
+//     example_d.zig:10:44: error: array literal requires address-of operator (&) to coerce to slice type '[]u8'
 //         const actual: []u8 = std.fmt.bytesToHex(input, .lower);
 //                              ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~
 test "bytesToHex" {
--- example_f.zig
+++ example_f.zig
@@ -2,10 +2,10 @@ const std = @import("std");
 
 // ❌ Fails
 //    % zig-0.12.0 test example_f.zig -freference-trace                                                                                                                                                                                                            (main)
-//    example_f.zig:5:39: error: expected type '[8]u8', found pointer
+//    example_f.zig:11:39: error: expected type '[8]u8', found pointer
 //        const actual: [input.len * 2]u8 = &std.fmt.bytesToHex(input, .lower);
 //                                          ^
-//    example_f.zig:5:39: note: address-of operator always returns a pointer
+//    example_f.zig:11:39: note: address-of operator always returns a pointer
 test "bytesToHex" {
     const input = "\xde\xad\xc0\xde";
     const actual: [input.len * 2]u8 = &std.fmt.bytesToHex(input, .lower);
